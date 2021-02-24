7f6eb3119a94d60d229485f22b670d0d1469c67e
vallimmateus <mateus.vallim@usp.br>
2021-02-24 12:37:05 -0300

    Começo da página de login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Perfil from "./images/Fotos.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="Container">
+			<div className="InContainer">
+				<img src={Perfil} className="Perfil" alt="perfil" />
+				<p>Email USP</p>
+				<input />
+			</div>
+		</div>
+	);
 }
 
 export default App;
